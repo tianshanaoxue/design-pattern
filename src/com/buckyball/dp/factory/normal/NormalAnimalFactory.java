ddbf44793ded577a7c3fef4d7524d43d1cbe1ae6
刘伟 <liuwei@tech.znhzg.com>
2018-06-24 22:50:13 +0800

    adjust dir

--- src/com/buckyball/dp/factory/normal/NormalAnimalFactory.java
+++ src/com/buckyball/dp/factory/normal/NormalAnimalFactory.java
@@ -1,5 +1,9 @@
 package com.buckyball.dp.factory.normal;
 
+import com.buckyball.dp.factory.AnimalCat;
+import com.buckyball.dp.factory.AnimalDog;
+import com.buckyball.dp.factory.IAnimal;
+
 public class NormalAnimalFactory {
 	
 	public IAnimal createAniman(String type){
--- src/com/buckyball/dp/factory/normal/Test.java
+++ src/com/buckyball/dp/factory/normal/Test.java
@@ -1,5 +1,7 @@
 package com.buckyball.dp.factory.normal;
 
+import com.buckyball.dp.factory.IAnimal;
+
 public class Test {
 
 	public static void main(String[] args) {
